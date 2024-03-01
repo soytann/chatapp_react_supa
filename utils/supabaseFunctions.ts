79dd6916d3f1ad218d736a9b4dddc021499066f1
あかり <>
2024-03-01 14:51:15 +0900

    やっとsupabase連携できた

---
+++ utils/supabaseFunctions.ts
@@ -0,0 +1,8 @@
+import { supabase } from "../utils/createClient";
+
+export const getAllUsers = async() => {
+    const users = await supabase
+        .from("users")
+        .select("*");
+    return users;
+}
