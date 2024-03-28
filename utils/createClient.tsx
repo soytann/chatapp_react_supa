feabf6d628cd995a3ee3c473c18cbe06a5c5d2a9
soytann <148407454+soytann@users.noreply.github.com>
2024-03-28 22:38:03 +0900

    Feature (#45)
    * deploy確認
    
    * 環境変数再度挑戦
    
    ---------
    
    Co-authored-by: あかり <>

--- utils/createClient.tsx
+++ utils/createClient.tsx
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../utils/types";
 
+const supabaseUrl = process.env.VITE_SUPABASE_URL as string;
+const supabaseKey = process.env.VITE_SUPABASE_KEY as string;
 
-export const supabase = createClient(
-    "https://coxmxhzthqwzffscyfdu.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNveG14aHp0aHF3emZmc2N5ZmR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyNjg0MDAsImV4cCI6MjAyNDg0NDQwMH0.x-gOaUW2dFLPl1-MU7SdZR-fgbTmex1iTvCQJZPzeZg"
-)
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+// export const supabase = createClient(
+//     // "https://coxmxhzthqwzffscyfdu.supabase.co",
+//     // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNveG14aHp0aHF3emZmc2N5ZmR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyNjg0MDAsImV4cCI6MjAyNDg0NDQwMH0.x-gOaUW2dFLPl1-MU7SdZR-fgbTmex1iTvCQJZPzeZg"
+// )
